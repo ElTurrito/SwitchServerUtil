7e08f52a340503cb0c4b303726a34d5761fbee3b
ElTurrito <faculautaroribo@gmail.com>
2021-12-25 22:57:21 -0300

    added to send a target player to a server

--- src/main/java/switchservers/switchserver/main/Switchserver/handlers/Command.java
+++ src/main/java/switchservers/switchserver/main/Switchserver/handlers/Command.java
@@ -1,5 +1,6 @@
 package switchservers.switchserver.main.Switchserver.handlers;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -17,19 +18,32 @@ public class Command implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player j = (Player) sender;
+       // if (sender instanceof Player) {
+                Player j = (Player) sender;
 
-            //SEND THE PLAYER TO THE SERVER
-            if (command.getName().equalsIgnoreCase("send")) {
+                //SEND THE PLAYER TO THE SERVER
                 if (args.length == 1) {
                     pluginMessage.connect(j, args[0]);
                 } else {
-                    j.sendMessage(Chat.translate("/send <server>") );
+                    j.sendMessage(Chat.translate("/send <server>"));
                 }
-            }
-        }
 
+
+                if (args.length == 3) {
+                    if (args[0].equalsIgnoreCase("send")) {
+
+                    }
+                    Player target = Bukkit.getPlayer(args[1]);
+                    if (target == null) {
+                        sender.sendMessage(Chat.translate("&cThe player '" + args[1] + "' is offline!"));
+                        return true;
+                    } else {
+                        pluginMessage.connect(target, args[2]);
+                        sender.sendMessage(Chat.translate("&aSucessfully sended the player " + target.getName() + " to the server" + args[2]));
+                    }
+                }
+            //}
+        //}
         return true;
     }
 }
